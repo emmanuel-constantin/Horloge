55ff8e68fe56d1fbb2bf47d506673c1b73bebbeb
emmanuel-constantin <emmanuel.constantin@laplateforme.io>
2023-03-16 15:34:43 +0100

    refonte totale du code pour afficher l'horloge sur une ligne, l'alarme ne fonctionne pas

--- main.py
+++ main.py
@@ -1,46 +1,42 @@
 import time
-import threading
 
-class Horloge:
-    def __init__(self, heure_initiale):
-        self.heure_debut = time.time()
-        self.heure_initiale = heure_initiale
-        self.alarme = None
-        self.alarme_activee = False
+# Fonction pour afficher l'heure actuelle
+def afficher_heure(heures, minutes, secondes):
+    print(f"\r{heures:02}:{minutes:02}:{secondes:02}", end="", flush=True)
 
-    def afficher_heure(self, heure_tuple):
-        self.heure_initiale = heure_tuple
-        self.heure_debut = time.time()
+# Demande à l'utilisateur de rentrer l'heure souhaitée
+heure_demandee = input("Entrez l'heure souhaitée (format hh:mm:ss) : ")
+heures, minutes, secondes = map(int, heure_demandee.split(':'))
 
-    def regler_alarme(self, heure_alarme):
-        self.alarme = heure_alarme
-        self.alarme_activee = True
+# Demande à l'utilisateur de programmer une alarme
+programmer_alarme = input("Voulez-vous programmer une alarme ? (o/n) : ")
+if programmer_alarme.lower() == "o":
+    heure_alarme = input("Entrez l'heure de l'alarme (format hh:mm:ss) : ")
+    heure_alarme = tuple(map(int, heure_alarme.split(':')))
+    alarme_programmee = True
+else:
+    alarme_programmee = False
 
-    def verifier_alarme(self):
-        if self.alarme_activee:
-            heure_actuelle = (self.heure_actuelle.tm_hour, self.heure_actuelle.tm_min, self.heure_actuelle.tm_sec)
-            if heure_actuelle == self.alarme:
-                print("L'alarme sonne !")
-                self.alarme_activee = False
-
-    def actualiser_heure(self):
-        while True:
-            time.sleep(1)
-            temps_ecoule = time.time() - self.heure_debut
-            heure_base = time.struct_time((1970, 1, 1, self.heure_initiale[0], self.heure_initiale[1], self.heure_initiale[2], 0, 0, 0))
-            timestamp = time.mktime(heure_base) + temps_ecoule
-            self.heure_actuelle = time.localtime(timestamp)
-            self.verifier_alarme()
-            print(f"{self.heure_actuelle.tm_hour:02d}:{self.heure_actuelle.tm_min:02d}:{self.heure_actuelle.tm_sec:02d}")
-
-heure_initiale = (16, 30, 0)
-horloge = Horloge(heure_initiale)
-thread = threading.Thread(target=horloge.actualiser_heure)
-thread.daemon = True
-thread.start()
-
-horloge.regler_alarme((16, 30, 10))
-
-# Le programme continuera de s'exécuter jusqu'à ce qu'il soit interrompu manuellement.
+# Boucle pour actualiser l'heure demandée toutes les secondes
 while True:
+    afficher_heure(heures, minutes, secondes)
     time.sleep(1)
+    secondes += 1
+    if secondes == 60:
+        secondes = 0
+        minutes += 1
+    if minutes == 60:
+        minutes = 0
+        heures += 1
+    if heures == 24:
+        heures = 0
+
+    # Vérification si l'heure actuelle correspond à l'heure demandée
+    heure_actuelle = time.localtime()
+    if heure_actuelle.tm_hour == heures and heure_actuelle.tm_min == minutes and heure_actuelle.tm_sec == secondes:
+        print("\nHeure demandée atteinte !")
+
+    # Vérification si l'heure actuelle correspond à l'heure de l'alarme programmée
+    if alarme_programmee and heure_actuelle.tm_hour == heure_alarme[0] and heure_actuelle.tm_min == heure_alarme[1] and heure_actuelle.tm_sec == heure_alarme[2]:
+        print("\nAlarme !")
+        break
